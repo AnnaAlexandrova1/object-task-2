04d14477584958ee4abcc6e83db8bb50c73c9a4a
Anna <anna4776497@gmail.com>
2022-04-18 15:49:49 +0300

    без ошибок

--- src/js/__tests__/orderByProps.test.js
+++ src/js/__tests__/orderByProps.test.js
@@ -1,7 +1,18 @@
 import orderByProps from '../orderByProps';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
-
-  expect(result).toBe(6);
+test('test print', () => {
+  const result = orderByProps({
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  }, ['name', 'level']);
+  expect(result).toEqual([
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ]);
 });
--- src/js/orderByProps.js
+++ src/js/orderByProps.js
@@ -1,41 +1,45 @@
 // TODO: write your code here
-export default function orderByProps(obj, priority){
-  if(priority === undefined || priority.lenght < 1){
-    throw new Error ('Не задана очередность элементов для печати')
-  }
-
+export default function orderByProps(obj, priority) {
   const sortable = [];
-  for( let key in obj){
-    sortable.push([key, obj[key]])
-  }
-
-  let byAlphabet = sortable.filter(element => priority.indexOf(element[0])<0); // возвращает массив значений, которых нет в Priority
-  let byPriority = sortable.filter(element => priority.indexOf(element[0])>=0) // возвращает массив значений из Priority
-
-  byAlphabet.sort(function(a, b) {
-    let x = a[0].toLowerCase();
-    let y = b[0].toLowerCase()
-    return x < y ? -1 : x > y ? 1 : 0;
-  })
-  
-  function sort (byPriority, priority) {
-    let arr = [];
-    for(let i = 0; i < priority.length; i++){
-      arr.push(byPriority.filter(element => element[0] === priority[i])[0])
+  for (const key in obj) {
+    if (obj.prototype.hasOwnProperty.call(obj, 'key')) {
+      sortable.push([key, obj[key]]);
     }
-    return arr
   }
-  let newPriority = sort (byPriority, priority);
-
-  newPriority = newPriority.concat(byAlphabet)
-  const arrSorted = []
-  newPriority.forEach(item => {
-    arrSorted.push(
-      {name: item[0],
-       value: item[1]
-      }
-    )
-  })
-  return arrSorted
+  // возвращает массив значений, которых нет в Priority
+  const byAlphabet = sortable.filter((element) => priority.indexOf(element[0]) < 0);
+  // возвращает массив значений из Priority
+  const byPriority = sortable.filter((element) => priority.indexOf(element[0]) >= 0);
+  byAlphabet.sort((a, b) => {
+    const x = a[0].toLowerCase();
+    const y = b[0].toLowerCase();
+    let res;
+    if (x < y) {
+      res = -1;
+    }
+    if (x > y) {
+      res = 1;
+    }
+    if (x === 0) {
+      res = 0;
+    }
+    return res;
+  });
+  function sort(byPriority, priority) {
+    const arr = [];
+    for (let i = 0; i < priority.length; i += 1) {
+      arr.push(byPriority.filter((element) => element[0] === priority[i])[0]);
+    }
+    return arr;
+  }
+  let newPriority = sort(byPriority, priority);
+  newPriority = newPriority.concat(byAlphabet);
+  const arrSorted = [];
+  newPriority.forEach((item) => {
+    arrSorted.push({
+      key: item[0],
+      value: item[1],
+    });
+  });
+  return arrSorted;
 }
-
